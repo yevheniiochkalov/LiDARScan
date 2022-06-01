dbfe3a40245897efff0c2dc730c765127bfa8611
mobiledev <yevheniiochkalov@gmail.com>
2022-06-01 08:35:11 +0300

    LidarScan app upload

---
+++ LiDARScan/MenuDataSource.swift
@@ -0,0 +1,69 @@
+//
+//  MenuDataSource.swift
+//  ExampleOfiOSLiDAR
+//
+//  Created by TokyoYoshida on 2021/01/31.
+//
+
+import UIKit
+
+struct MenuItem {
+    let title: String
+    let description: String
+    let prefix: String
+    
+    func viewController() -> UIViewController {
+        let storyboard = UIStoryboard(name: prefix, bundle: nil)
+        let vc = storyboard.instantiateInitialViewController()!
+        vc.title = title
+
+        return vc
+    }
+}
+
+class MenuViewModel {
+    private let dataSource = [
+        MenuItem (
+            title: "Depth Map",
+            description: "Display the depth map on the screen.",
+            prefix: "DepthMap"
+        ),
+        MenuItem (
+            title: "Confidence Map",
+            description: "Display the confidence on the screen.",
+            prefix: "ConfidenceMap"
+        ),
+        MenuItem (
+            title: "Collision",
+            description: "Collision detection of objects using LiDAR.",
+            prefix: "Collision"
+        ),
+        MenuItem (
+            title: "Export",
+            description: "Export scaned object to .obj file.",
+            prefix: "Export"
+        ),
+        MenuItem (
+            title: "Scan with Texture",
+            description: "Scan object with color texture.",
+            prefix: "Scan"
+        ),
+        MenuItem (
+            title: "Point Cloud",
+            description: "Point cloud.",
+            prefix: "PointCloud"
+        )
+    ]
+    
+    var count: Int {
+        dataSource.count
+    }
+    
+    func item(row: Int) -> MenuItem {
+        dataSource[row]
+    }
+    
+    func viewController(row: Int) -> UIViewController {
+        dataSource[row].viewController()
+    }
+}
